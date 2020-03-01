3fd58a4e774dbb6dbe56889d19726fd730966af8
LazyCrazyTom <tclare50@gmail.com>
2020-03-01 01:40:17 +0000

    Video upload

--- WebPortal.py
+++ WebPortal.py
@@ -1,7 +1,22 @@
 from flask import Blueprint, render_template
+from flask import request
+from web_app.TransloaditInterface import TransloaditInterface
 
 web_portal = Blueprint('web_portal', __name__)
 
+
 @web_portal.route("/displayModule")
 def displayModule():
-    return render_template("webportal.html", title="Artificial Intelligence")
+    module_id = request.args.get("id")
+    title = "Artificial Intelligence" # actually use module ID for this bit
+    videos = [["Welcome - L01", "45:12", "/pics/23/01.jpg"], ["Use Cases - L02", "52:44", "/pics/23/02.jpg"]]
+    videos_len = len(videos)
+    return render_template("lecture/module.html", title=title, videos=videos, videos_len=videos_len)
+
+
+@web_portal.route("/deliver")
+def deliverModule():
+    module_id = request.args.get("id")
+    title = "Artificial Intelligence" # actually use module ID for this bit
+    return render_template("lecture/deliver.html", title=title)
+
